c624aa46175570b89585165d60114f1010dbbf48
Dima <2008Nerush2008@ukr.net>
2016-09-04 16:00:33 +0300

    add word class

--- src/main/java/com/crossword/ConsoleRunner.java
+++ src/main/java/com/crossword/ConsoleRunner.java
@@ -4,6 +4,7 @@ public class ConsoleRunner {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        Word obj1 = new Word("Hello, Timur!");
+        System.out.println(obj1.getWord());
     }
-
 }
---
+++ src/main/java/com/crossword/Word.java
@@ -0,0 +1,18 @@
+package com.crossword;
+
+/**
+ * Created by Sky_el on 04.09.2016.
+ */
+public class Word {
+    private String word;
+
+    Word(String str)
+    {
+        word = str;
+    }
+
+    String getWord()
+    {
+        return word;
+    }
+}
